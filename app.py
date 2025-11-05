2f92946116d2ece2ef3055d023b1c681ac66863e
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-05 21:29:13 +0100

    Importacion de librerias y lectura de csv

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 #¿Cual seria el precio de venta al que tendria
 #que poner mi vivienda para que este alineado con el mercado?
 
+#1-Importacion de librerias
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#2-Importacion de fuentes de datos
+data = pd.read_csv(r'files\houses_Madrid.csv')
+print(data.head())
